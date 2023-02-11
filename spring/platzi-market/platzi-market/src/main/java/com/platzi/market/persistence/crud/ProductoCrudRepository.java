68d43fe0c6bf4e9ca65c6c0491f53d42f6d5a0e2
Ariel F <ariel.incgarcia@gmail.com>
2023-02-11 01:29:11 -0800

    Spring: query methods

--- spring/platzi-market/platzi-market/src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ spring/platzi-market/platzi-market/src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,18 @@
 package com.platzi.market.persistence.crud;
 
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
+    //ESTA ES LA FORMA DE BUSCAR INFORMACION DE MANERA NATIVA CON QUERY
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?",nativeQuery = true)
+    //List<Producto> findByCategoria(int idCategoria);
+
+    //UTILIZANDO QUERY METHODS
+    List<Producto> findByIdCategoriaOrderByNombre(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
